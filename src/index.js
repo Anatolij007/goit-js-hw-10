2074054ab8c70747ca97f3788e6896babd0b7755
Anatolij007 <98270281+Anatolij007@users.noreply.github.com>
2022-08-31 22:44:26 +0300

    не працює

--- src/index.js
+++ src/index.js
@@ -1,3 +1,94 @@
-import './css/styles.css';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// // import Notiflix from 'notiflix';
+// import debounce from 'lodash.debounce';
 
-const DEBOUNCE_DELAY = 300;
+// import { fetchCountries } from './fetchCountries';
+// import './css/styles.css';
+
+// const DEBOUNCE_DELAY = 300;
+
+// const search = document.querySelector('#search-box');
+// const list = document.querySelector('.country-list');
+// const info = document.querySelector('.country-info');
+// const accessMessage =
+//   'Too many matches found. Please enter a more specific name.';
+// const errorMessage = 'Oops, there is no country with that name';
+
+// // вішаеєм слухача і за допомогою пакета lodash.debounce запити будуть виконуватися 1 раз в 300мс
+// search.addEventListener('input', debounce(inputCountry, DEBOUNCE_DELAY));
+
+// // функція пошуку країн
+// function inputCountry(event) {
+//   // clearCountries();
+
+//   //вирішує проблеми с пробілами
+//   const removeSpaces = event.targer.value.trim();
+
+//   if (!removeSpaces) {
+//     clearCountries();
+//     return;
+//   }
+//   fetchCountries(removeSpaces)
+//     .then(countries => {
+//       // якщо більше 10 символів, введіще ще
+//       if (countries.length > 10) {
+//         clearCountries();
+//         return Notify.info(accessMessage);
+//       }
+//       // якщо більше від 2 до 10 символів, покажи розмітку
+//       if (countries.length >= 2 && countries.length <= 10) {
+//         return renderCountries(countries);
+//       }
+//       // якщо 1, покажи повністю країну
+//       if (countries.length === 1) {
+//         return renderCountry(countries);
+//       }
+//     })
+//     .catch(error => Notify.failure(errorMessage));
+// }
+
+// // функція рендерить 2-10 країн
+
+// function renderCountries(countries) {
+//   const markup = countries
+//     .map(({ name, flags }) => {
+//       return `<li><img class="country-img"
+//       src="${flags.svg}"
+//       alt="Flag of${name.official}
+//       width="30"> &nbsp
+//       <h1 class="country-name">${name.official}</h1></li>`;
+//     })
+//     .join('');
+
+//   info.insertAdjacentHTML('beforeend', markup);
+// }
+
+// // функція рендерить розмітку країни
+// function renderCountry(country) {
+//   const markup = country
+//     .map(({ name, capital, population, flags, languages }) => {
+//       return `<div class="country-div">
+//       <img class="country-img"
+//       src="${flags.svg}"
+//       alt="Flag of${name.official}
+//       width="30"> &nbsp
+//       <h1 class="country-name">${name.official}</h1>
+//       <p class="country-capital"><b>Capital:</b>${capital}</p>
+//       <p class="country-population"><b>Population:</b>${population}</p>
+//       <p class="country-languages"><b>languages:</b>${Object.values(
+//         languages
+//       )}</p>
+//     </div>`;
+//     })
+//     .join('');
+
+//   info.insertAdjacentHTML('beforeend', markup);
+// }
+
+// //очищює поле країн
+// function clearCountries() {
+//   list.innerHTML = '';
+//   info.innerHTML = '';
+// }
+
+// =============================================
